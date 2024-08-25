9a0b6bb9317db1e72ca76da38b03e4b8d3f2a241
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-08-25 13:27:26 -0400

    Update and rename a14-mitho.sh to script.sh

---
+++ script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/euclidTeam/manifest.git -b qpr3 --git-lfs --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/SoumikBar/local_manifests -b EuclidOS-14 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DEV•BeingEuclid
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch euclid_Mi439_4_19-ap2a-userdebug
+make installclean
+echo "============="
+
+# Build ROM
+m euclid
