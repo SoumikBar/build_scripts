697e587cb715f6c15f6341b188c7bbf3d1ba6cc2
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-08-26 02:18:10 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/SoumikBar/local_manifests -b EuclidOS-14 .repo/local_manifests
+git clone https://github.com/SoumikBar/local_manifests -b ProjectEverest-14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
